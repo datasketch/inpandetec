cd88334361bd3bea5cab85f814a597a41b3a6f96
CamilaAchuri <camilaachury.4@gmail.com>
2022-11-25 12:14:14 -0300

    add app skeleton

---
+++ inst/inpandetec-app/app.R
@@ -0,0 +1,112 @@
+library(inpandetec)
+library(shiny)
+library(parmesan)
+
+ui <-
+  fluidPage(
+    tags$head(
+      tags$link(rel="stylesheet", type="text/css", href="custom.css")
+    ),
+    div(class = "layout-container",
+        div(class = "layout-panels",
+            div(class = "app-container",
+                div(class = "panel top-malibu",
+                    div (class = "panel-body",
+                         uiOutput("controls")
+                    )),
+                div(class = "panel",
+                    div (class = "panel-body",
+                         div(style="flex-grow: 1; min-width: 600px;",
+                             div(class = "head-viz",
+                                 div(style = "display:flex;gap:20px;margin-bottom: 20px;align-items: flex-end;",
+                                     "VISUALIZACIÓN",
+                                     "viz"),
+                                 "descarga"),
+                             div(class = "viz-nucleo",
+                                 "viz"
+                             )
+                         )
+                    )),
+                div(class = "panel",
+                    div (class = "panel-body",
+                         div(style="flex-grow: 1; min-width: 320px;",
+                             "info extra")
+                    )
+                )
+            )
+        )
+    )
+  )
+
+
+server <-
+  function(input, output, session) {
+
+    pickerOpts <- reactive({
+      list(
+        `actions-box` = TRUE,
+        `deselect-all-text` = "Ninguno",
+        `select-all-text` = "Todos",
+        title = "Todos"
+      )
+    })
+
+    types <- reactive({
+      unique(data_to_app$`Tipo de violencia experimentada`)
+    })
+
+    defaultType <- reactive({
+      req(types())
+      types()[1]
+    })
+
+    freq <- reactive({
+      unique(data_to_app$Frecuencia)
+    })
+
+
+    countries <- reactive({
+      unique(data_to_app$País)
+    })
+
+
+    genero <- reactive({
+      unique(data_to_app$`Identidad de género`)
+    })
+
+    orientation <- reactive({
+      unique(data_to_app$`Orientación sexual`)
+    })
+
+    min_age <- reactive({
+      min(data_to_app$Edad, na.rm = T)
+    })
+
+    max_age <- reactive({
+      max(data_to_app$Edad, na.rm = T)
+    })
+
+    range_age  <- reactive({
+      req(min_age())
+      req(max_age())
+      c(min_age(), max_age())
+    })
+
+
+    # Renderizar inputs con parmesan ------------------------------------------
+
+    parmesan <- parmesan_load()
+    parmesan_input <- parmesan_watch(input, parmesan)
+
+    output_parmesan("controls",
+                    input = input, output = output, session = session,
+                    env = environment())
+
+
+}
+
+
+shinyApp(ui, server)
+
+
+
